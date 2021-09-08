448e3938c08671c162630ecc7d7da8af822375aa
Zachary McCray <z182m094@ku.edu>
2021-09-08 12:50:04 -0500

    fixed search function

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -41,9 +41,21 @@ int LinkedList<T>::size() const
 template <typename T>
 bool LinkedList<T>::search(T value) const
 {
-	Node<T>* temp = m_front;
-	bool isFound = false;
-	return(isFound);
+        Node<T>* temp = m_front;
+        bool isFound = false;
+        while(temp != nullptr)
+	{
+                if(temp->getValue() == value)
+                {
+                        isFound == true;
+                        return(isFound)
+                }
+                else
+                {
+                temp = temp->getNext();
+                }
+	}
+        return(isFound);
 }
 
 template <typename T>
